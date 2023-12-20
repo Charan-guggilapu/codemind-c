e0e0071ebcfab5710fed036ebfc466e7bcecffc2
Charan-guggilapu <147476551+Charan-guggilapu@users.noreply.github.com>
2023-12-21 03:36:54 +0530

    #include<stdio.h> int main() {     int a,i,j,k,l;     scanf("%d",&a);     for(i=a;i>=1;i--){         for(k=a;k>i;k--){             printf(" ");         }         for(j=1;j<=i;j++){             printf("* ");         }         for(l=j-i;l>1;l--){         printf("* ");         }         printf(" ");     } }

---
+++ Pattern_7.1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,j,k,l;
+    scanf("%d",&a);
+    for(i=a;i>=1;i--){
+        for(k=a;k>i;k--){
+            printf(" ");
+        }
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+        for(l=j-i;l>1;l--){
+        printf("* ");
+        }
+        printf("
+");
+    }
+}
