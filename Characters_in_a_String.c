70bf1e01acc8f6b1f5e1b1d0abbf4edebbabe356
Charan-guggilapu <147476551+Charan-guggilapu@users.noreply.github.com>
2024-01-22 11:03:42 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100000];     int i;     scanf("%[^ ]s",str);     int len=strlen(str);
            printf("%d",len);
        
    }

---
+++ Characters_in_a_String.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100000];
+    int i;
+    scanf("%[^
+]s",str);
+    int len=strlen(str);
+    
+        printf("%d",len);
+    
+}
